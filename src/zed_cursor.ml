bc565b3e8fdd19a095a6c6e979207379309e70b4
Jeremie Dimino <jdimino@janestreet.com>
2014-11-21 15:26:32 +0000

    fix invlalid use of react (related to diml/utop#114)

--- src/zed_cursor.ml
+++ src/zed_cursor.ml
@@ -11,12 +11,15 @@ open React
 
 exception Out_of_bounds
 
+type action =
+  | User_move of int
+  | Text_modification of (int * int * int) (* start, added, removed *)
+
 type t = {
   position : int signal;
-  set_position : int -> unit;
-  mutable length : int;
+  send : action -> unit;
+  length : int ref;
   changes : (int * int * int) event;
-  mutable handle_changes : unit event;
   get_lines : unit -> Zed_lines.t;
   coordinates : (int * int) signal;
   line : int signal;
@@ -27,53 +30,58 @@ type t = {
 
 let create length changes get_lines position wanted_column =
   if position < 0 || position > length then raise Out_of_bounds;
-  let position, set_position = S.create position in
-  let wanted_column, set_wanted_column = S.create wanted_column in
+  let length = ref length in
+  let user_moves, send = E.create () in
+  let update_position position action =
+    match action with
+    | User_move pos -> pos
+    | Text_modification (start, added, removed) ->
+      let delta = added - removed in
+      length := !length + delta;
+      if !length < 0 then raise Out_of_bounds;
+      (* Move the cursor if it is after the start of the changes. *)
+      if position > start then begin
+        if delta >= 0 then
+          (* Text has been inserted, advance the cursor. *)
+          position + delta
+        else if position < start - delta  then
+          (* Text has been removed and the removed block contains the
+             cursor, move it at the beginning of the removed block. *)
+          start
+        else
+          (* Text has been removed before the cursor, move back the
+             cursor. *)
+          position + delta
+      end else
+        position
+  in
+  let text_modifications = E.map (fun x -> Text_modification x) changes in
+  let position =
+    S.fold update_position position (E.select [user_moves; text_modifications])
+  in
   let compute_coordinates position =
     let lines = get_lines () in
     let index = Zed_lines.line_index lines position in
     (index, position - Zed_lines.line_start lines index)
   in
   let coordinates = S.map compute_coordinates position in
-  let cursor = {
+  let wanted_column, set_wanted_column = S.create wanted_column in
+  {
     position;
-    set_position;
+    send;
     length;
     changes;
-    handle_changes = E.never;
     get_lines;
     coordinates;
     line = S.map fst coordinates;
     column = S.map snd coordinates;
     wanted_column;
     set_wanted_column;
-  } in
-  let handle_changes (start, added, removed) =
-    let delta = added - removed in
-    cursor.length <- cursor.length + delta;
-    if cursor.length < 0 then raise Out_of_bounds;
-    let position = S.value position in
-    (* Move the cursor if it is after the start of the changes. *)
-    if position > start then begin
-      if delta >= 0 then
-        (* Text has been inserted, advance the cursor. *)
-        set_position (position + delta)
-      else if position < start - delta  then
-        (* Text has been removed and the removed block contains the
-           cursor, move it at the beginning of the removed block. *)
-        set_position start
-      else
-        (* Text has been removed before the cursor, move back the
-           cursor. *)
-        set_position (position + delta)
-    end
-  in
-  cursor.handle_changes <- E.map handle_changes changes;
-  cursor
+  }
 
 let copy cursor =
   create
-    cursor.length
+    !(cursor.length)
     cursor.changes
     cursor.get_lines
     (S.value cursor.position)
@@ -93,17 +101,17 @@ let set_wanted_column cursor column = cursor.set_wanted_column column
 
 let move cursor ?(set_wanted_column=true) delta =
   let new_position = S.value cursor.position + delta in
-  if new_position < 0 || new_position > cursor.length then
+  if new_position < 0 || new_position > !(cursor.length) then
     raise Out_of_bounds
   else begin
-    cursor.set_position new_position;
+    cursor.send (User_move new_position);
     if set_wanted_column then cursor.set_wanted_column (S.value cursor.column)
   end
 
 let goto cursor ?(set_wanted_column=true) position =
-  if position < 0 || position > cursor.length then
+  if position < 0 || position > !(cursor.length) then
     raise Out_of_bounds
   else begin
-    cursor.set_position position;
+    cursor.send (User_move position);
     if set_wanted_column then cursor.set_wanted_column (S.value cursor.column)
   end
