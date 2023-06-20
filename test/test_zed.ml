aa291c8317e5d0e82478b34e9868438373da1856
Etienne Millon <me@emillon.org>
2023-06-20 15:37:55 +0200

    Add new test

--- test/test_zed.ml
+++ test/test_zed.ml
@@ -56,4 +56,19 @@ let test_of_utf8 =
   ; test_invalid ~name:"U+DFFF" "\xed\xbf\xbf"
   ])
 
-let () = Alcotest.run "zed" [test_next_error; test_of_utf8]
+let test_kill_next_word =
+  let test =
+    ("kill_next_word", `Quick, fun () ->
+      (* Test that [kill_next_word] does not raise [Out_of_bounds] *)
+      let engine = Zed_edit.create () in
+      let cursor = Zed_edit.new_cursor engine in
+      let ctxt = Zed_edit.context engine cursor in
+      Zed_edit.insert ctxt (Zed_rope.of_string (Zed_string.of_utf8 "hello"));
+      Zed_edit.set_mark ctxt;
+      Zed_edit.insert ctxt (Zed_rope.of_string (Zed_string.of_utf8 " world"));
+      Zed_edit.goto_mark ctxt;
+      Zed_edit.kill_next_word ctxt)
+  in
+  ( "kill_next_word", [ test ] )
+
+let () = Alcotest.run "zed" [test_next_error; test_of_utf8; test_kill_next_word]
